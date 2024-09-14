d1c57e68ef94d86aec9805ec2fdc8b281e8e00aa
ship123talk456 <158138042+ship123talk456@users.noreply.github.com>
2024-09-14 13:25:20 +0800

    Update shipview.py

--- shipview.py
+++ shipview.py
@@ -19,17 +19,16 @@ def main():
     # 读取CSV文件
     df = load_ship_data(filename)
     
-    # 创建一个列表框来显示船舶信息
-    col1, col2 = st.columns([1, 4])  # 左侧列表占五分之一，右侧地图占五分之四
+    # 创建两个列，左侧列表占五分之一，右侧地图占五分之四
+    col1, col2 = st.columns([1, 4])
+    
     with col1:
-        st.write("船舶列表：")
-        # 显示船舶信息的列表框
-        for index, row in df.iterrows():
-            st.write(f"IMO号: {row['IMO number']}, 船舶名称: {row['Name of ship']}, 总吨位: {row['Gross tonnage']}, 建造年份: {row['Year of build']}, 船旗: {row['Flag']}")
+        st.write("## 船舶列表")
+        # 显示船舶信息的表格
+        st.dataframe(df)
 
     with col2:
-        # 这里将添加地图控件的代码
-        st.write("船舶实时位置图：")
+        st.write("## 船舶实时位置图")
         # 由于没有提供实时位置的API，这里暂时留空
         # 一旦提供了API，可以使用例如st.map()或者folium地图来显示船舶位置
 
